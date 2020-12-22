4b8351c800a18ff81dd95cf1c5871b04927e7f46
Jay Idrees <idreesjay@gmail.com>
2020-12-22 16:37:25 -0500

    Added code for to display users and a message if there are no users to UsersList.js

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,8 +1,24 @@
 import React from "react";
-
+import UserItem from "./UserItem"
 import "./UsersList.css"
 
 const UsersList=props=>{
+
+    if (props.items.length===0){
+
+        return(
+            <div>
+                <h2>No Users found</h2>
+            </div>
+        );
+    } // br close if
+
+    return <ul>
+        {props.items.map (user=>
+        {return <userItem />;
+         })}
+    </ul>
+
 
 };
 
