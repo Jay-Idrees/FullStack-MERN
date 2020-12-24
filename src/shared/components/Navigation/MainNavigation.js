87fec39987bd77088b10631556f220414d7ffa1e
Jay Idrees <idreesjay@gmail.com>
2020-12-23 20:10:49 -0500

    Using the useState method in react to set the state drawerIsOpen

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState}from "react";
 import {Link} from "react-router-dom";
 import NavLinks from "./NavLinks"
 import SideDrawer from "./SideDrawer"
@@ -8,15 +8,18 @@ import "./MainNavigation.css";
 
 
 const MainNavigation=props=>{
+    const [drawerIsOpen, setDrawerIsOpen]=useState(false)
 return(
- 
+
     // Can only have ONE root JSX element per component-React/JS Rule if we want to use multiple then we should include React.Fragment
     <React.Fragment>
+    {drawerIsOpen && (
     <SideDrawer>
         <nav className="main-navigation__drawer-nav">
             <NavLinks />
         </nav>
     </SideDrawer>
+        )}
     {/*Props.children will return anything between the <MainHeader></Mainheader> to the MainHeader.js */}
         <MainHeader>
             <button className="main-navigation__menu-btn">
