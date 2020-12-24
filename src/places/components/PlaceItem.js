224d8443ea116585b740c12bff44eb4784bedd14
Jay Idrees <idreesjay@gmail.com>
2020-12-24 15:17:52 -0500

    Added code for PlaceItem.js by using props. Created buttons for view on map, Edit, Delete

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,8 +3,24 @@ import React from "react";
 
 import "./PlaceItem.css"
 
+// Passing data into the actual component for use
 const PlaceItem=props=>{
-    return
+    return<li className='place-item'>
+        <div className="place-item__image">
+            <img src={props.image}  alt={props.title}/>
+        </div>
+        <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+        </div>
+        <div className="place-item__actions">
+            <button> View on Map</button>
+            <button> Edit </button>
+            <button> Delete </button>
+
+        </div>
+    </li>
 };
 
 export default PlaceItem
--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,9 +1,35 @@
 import React from "react"
+import Card from "../../shared/components/UIElements/Card";
 
 import "./PlaceList.css"
 
 const PlaceList=props=>{
-    
+    if (props.items.length===0){
+        <div className='place-list center'>
+            <Card>
+                <h2>No Places Found. Maybe create one?</h2>
+                <button>Share Place</button>
+            </Card>
+        </div>
+    }
+    return (
+        <ul className='place-list'>
+
+            {props.items.map(place=>(
+            <PlaceItem
+            key={place.id}
+            id={place.id}
+            image={place.imageURL}
+            title={place.title}
+            description={place.description}
+            address={place.address}
+            creatorId={place.creator}
+            coordinates={place.location}
+            />
+
+            ))}
+        </ul>
+    )
 };
 
 export default PlaceList
