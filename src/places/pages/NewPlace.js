e61f82f1e14d47cb8d7df0d2b21ec2f1da019e16
Jay Idrees <idreesjay@gmail.com>
2020-12-28 04:08:37 -0500

    Added functionality to apply Validator_require validation onto the title textbox in the form using the useReducer hook

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Input from "../../shared/FormElements/Input"
-
+import {VALIDATOR_REQUIRE} from "../../shared/util/validators"
 import "./NewPlace.css";
 const NewPlace=()=>{
 
@@ -11,8 +11,8 @@ const NewPlace=()=>{
         element="input" 
         type="text" 
         label="Title" 
-        validators={[]} 
-        // onInput={} 
+        //The piece of code below passing of VALIDATOR_REQIORE from the validation.js as props. It is basically a function to be applied onto the input valuse from this particular form input 
+        validators={[{VALIDATOR_REQUIRE}]} 
         errorText="Please enter a valid title"/>
 
     </form>);
